8d2cbe87daf7a1049842f2d6bf1e43d3e30f4240
martins <martinohez@gmail.com>
2023-12-09 01:54:30 +0100

    nav update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,35 +8,30 @@ const Nav = () => {
     <nav className="sticky bottom-2 lg:bottom-8 w-full overflow-hidden z-50 lg:mt-10">
       <div className="container mx-auto">
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
-          <NavLink
-            to="/"
-            className="w-[60px] h-[60px] flex items-center justify-center"
-          >
-            <BiHomeAlt />
+          <NavLink to="/">
+            <div className="w-[60px] h-[60px] flex items-center justify-center">
+              <BiHomeAlt />
+            </div>
           </NavLink>
-          <NavLink
-            to="/about"
-            className="w-[60px] h-[60px] flex items-center justify-center"
-          >
-            <BiUser />
+          <NavLink to="/about">
+            <div className="w-[60px] h-[60px] flex items-center justify-center">
+              <BiUser />
+            </div>
           </NavLink>
-          <NavLink
-            to="/services"
-            className="w-[60px] h-[60px] flex items-center justify-center"
-          >
-            <BsClipboardData />
+          <NavLink to="/services">
+            <div className="w-[60px] h-[60px] flex items-center justify-center">
+              <BsClipboardData />
+            </div>
           </NavLink>
-          <NavLink
-            to="/work"
-            className="w-[60px] h-[60px] flex items-center justify-center"
-          >
-            <BsBriefcase />
+          <NavLink to="/work">
+            <div className="w-[60px] h-[60px] flex items-center justify-center">
+              <BsBriefcase />
+            </div>
           </NavLink>
-          <NavLink
-            to="/contact"
-            className="w-[60px] h-[60px] flex items-center justify-center"
-          >
-            <BsChatSquareText />
+          <NavLink to="/contact">
+            <div className="w-[60px] h-[60px] flex items-center justify-center">
+              <BsChatSquareText />
+            </div>
           </NavLink>
         </div>
       </div>
