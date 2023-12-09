aed5e7578f0826b5f84d8a4bc1668d4905a5762b
martins <martinohez@gmail.com>
2023-12-09 01:40:22 +0100

    updated the landing page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,31 +10,31 @@ const Nav = () => {
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
           <NavLink
             to="/"
-            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            className="w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiHomeAlt />
           </NavLink>
           <NavLink
             to="/about"
-            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            className="w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiUser />
           </NavLink>
           <NavLink
             to="/services"
-            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            className="w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsClipboardData />
           </NavLink>
           <NavLink
             to="/work"
-            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            className="w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsBriefcase />
           </NavLink>
           <NavLink
             to="/contact"
-            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            className="w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsChatSquareText />
           </NavLink>
